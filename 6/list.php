9fbc4454ab138c2330cf4f4543c015a2f6444adf
maax101 <belous.maxim@gmail.com>
2017-08-09 18:13:31 +0300

    Add files via upload

--- 6/list.php
+++ 6/list.php
@@ -2,6 +2,21 @@
 $test_list=[];
 $test_dir = opendir('tests/');
 //print_r (scandir('tests/'));
+//var_dump(pathinfo(readdir($test_dir)));
+//var_dump($_GET);
+
+while (false !== ($entry = readdir($test_dir))) {
+    if ($entry != "." && $entry != ".." && pathinfo($entry,PATHINFO_EXTENSION)=='json') {
+    $test_list[]= pathinfo($entry, PATHINFO_FILENAME);
+    //var_dump(pathinfo($entry));
+
+    }
+
+}
+ 
+
+ 
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -11,15 +26,11 @@ $test_dir = opendir('tests/');
 <body>
 	<p><h2>Список тестов:</h2></p>
 <?php
-while (false !== ($entry = readdir($test_dir))) {
-    if ($entry != "." && $entry != ".." && pathinfo($entry,PATHINFO_EXTENSION)=='json') {
-    $test_list[]= $entry;
-    }
-}
+
 foreach ($test_list as $name) {
 	echo '<br><li>'.$name.'</li>
 	<form action="test.php" method="GET">
-		<input type="hidden" name="test_name" value="'.$name.'">
+		<input type="hidden" name="test_name" value="'.$name.'.json">
 		<input type="submit" value="пройти тест">
 	</form>';
 }
--- 6/test.php
+++ 6/test.php
@@ -1,10 +1,13 @@
 <?php 
-if (isset($_GET['test_name'])) {
+if (!isset($_GET['test_name']) || !file_exists('tests/'.$_GET['test_name'])) {
+	header("HTTP/1.1 404 Not Found",$replace=true,http_response_code(404));
+	exit();	
+} else { 
 	$path_file = 'tests/'.$_GET['test_name'];
 	$temp = file_get_contents($path_file);
 	file_put_contents('temp.php', json_decode($temp));
 	$test = file_get_contents('temp.php');
-} else { header('<h1>HTTP/1.0 404 Not Found</h1>',404); }
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,7 +17,7 @@ if (isset($_GET['test_name'])) {
 <body>
 	<form action="result1.php" method="GET">
 		<input type="text" name="user_name" value="" placeholder="введите свое имя..." required="введите имя">
-		<?php echo $test; ?>
+		<?=$test?>
 		<input type="submit" value="результат">
 	</form>	
 </body>
